97888b5d7180eae71f68c86442ccd56742603270
Tomohiro Kumagai <tomo@ez-net.jp>
2023-09-06 14:50:31 +0900

    Refactor code.
    - Replace `if` with `switch`.
    - Extract numeronimize feature into a separate function.

--- Numeronyms Extension/Resources/content.js
+++ Numeronyms Extension/Resources/content.js
@@ -3,34 +3,38 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     toNumeronym(document.body);
 });
 
+const ingoringTagNames = ['SCRIPT', 'NOSCRIPT', 'STYLE'];
+const numeronymPattern = /\b([a-z])([a-z]+)([a-z])\b/gi;
+
 function toNumeronym(node) {
     
-    if (node.nodeType == Node.TEXT_NODE) {
-        
-        const text = node.textContent;
-        const pattern = /\b([a-z])([a-z]+)([a-z])\b/gi;
-        
-        if (pattern.test(text)) {
+    switch (node.nodeType) {
             
-            const numeronymedHTML = text.replace(pattern, (text, first, middles, last) => {
-                
-                const lengthOfMiddles = middles.length;
-                return `<span title="${text}">${first}${lengthOfMiddles}${last}</span>`;
-            });
-            
-            const newNode = document.createElement('span');
-            newNode.innerHTML = numeronymedHTML;
-            
-            node.parentNode.replaceChild(newNode, node);
-        }
-    } else {
-        const ingoringTagNames = ['SCRIPT', 'NOSCRIPT', 'STYLE'];
+    case Node.TEXT_NODE:
+        numeronimize(node);
         
+    default:
         for (const child of node.childNodes) {
-            
             if (ingoringTagNames.includes(child.tagName)) continue;
-            
             toNumeronym(child);
         }
     }
+}
+
+function numeronimize(textNode) {
+    
+    const text = textNode.textContent;
+    
+    if (!numeronymPattern.test(text)) return;
+
+    const numeronymedHTML = text.replace(numeronymPattern, (text, first, middles, last) => {
+        
+        const lengthOfMiddles = middles.length;
+        return `<span title="${text}">${first}${lengthOfMiddles}${last}</span>`;
+    });
+    
+    const numeronymedNode = document.createElement('span');
+    numeronymedNode.innerHTML = numeronymedHTML;
+    
+    textNode.parentNode.replaceChild(numeronymedNode, textNode);
 }
